6ba8782d4dc3525425db1fc225d8773c7851921d
FerBreton <fernanda.bretonq@gmail.com>
2020-12-05 22:27:36 -0600

    creación de modelos

---
+++ PaseDeLista_project/pages/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+#modelo de alumno
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+#modelo de clase
+class Clase(models.Model):
+    fecha_clase = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.fecha_clase
+
+#modelo de materia
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50)
+    periodo = models.CharField(max_length=30)
+    year = models.IntegerField(default = 0)
+    clase = models.ForeignKey(Clase, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+
+#modelo de profesor
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=50)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+#modelo de asistencia
+class Asistencia(models.Model):
+    clase = models.ForeignKey(Clase, on_delete=models.CASCADE)
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    fecha_asistencia = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.fecha_asistencia
+
---
+++ PaseDeLista_project/pages/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views import View 
+#from .models import Publication, Autor, Comentario 
+#from .forms import PublicationForm
+
+# Create your views here.
+
+class home(View):
+    def get(self, request):
+        publicaciones = Publication.objects.all()
+
+        #impresion en consola de las publicaciones
+        for pub in publicaciones:
+            print(pub)
+
+        context = {'publicaciones' : publicaciones}
+        return render(request, 'index.html', context)
+
+class PublicationAdd(View):
+    def get(self, request):
+        form = PublicationForm()
+        context = {'form' : form}
+        return render(request, 'publication.html', context)
+    
+    def post(self, request):
+        form = PublicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = PublicationForm()
+            context = {'form' : form}
+            return render(request, 'publication.html', context)
+
+class PublicationUpdate(View):
+    def get(self, request, id):
+        publicacion = Publication.objects.get(id=id)
+        form = PublicationForm(instance = publicacion)
+        context = {'form' : form}
+        return render(request, 'publication.html', context)
+    
+    def post(self, request, id):
+        publicacion = Publication.objects.get(id=id)
+        form = PublicationForm(request.POST, instance = publicacion)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            context = {'form' : form}
+            return render(request, 'publication.html', context)
